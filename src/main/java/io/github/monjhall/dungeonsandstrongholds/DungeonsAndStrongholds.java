e56f47da318913f0786aedaf1e0f3eb34e14cc4b
Thomas Lourenco <TSLourenco@WPI.edu>
2018-05-17 23:33:02 -0400

    Committing starting files. Some of these should probably be in the ignore, but meh.

---
+++ src/main/java/io/github/monjhall/dungeonsandstrongholds/DungeonsAndStrongholds.java
@@ -0,0 +1,16 @@
+package io.github.monjhall.dungeonsandstrongholds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DungeonsAndStrongholds extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable is called!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("onDisable is called!");
+    }
+
+}
